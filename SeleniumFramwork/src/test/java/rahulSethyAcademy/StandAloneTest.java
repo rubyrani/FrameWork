e2cd34df2927e617d0a25b933b639ab393865f9d
ruby <ruby@192.168.1.36>
2022-10-17 23:51:45 +0530

    commited succiful

---
+++ SeleniumFramwork/src/test/java/rahulSethyAcademy/StandAloneTest.java
@@ -0,0 +1,27 @@
+package rahulSethyAcademy;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class StandAloneTest {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/client");
+		driver.findElement(By.id("userEmail")).sendKeys("rubyrani613@gmail.com");
+		driver.findElement(By.id("userPassword")).sendKeys("Ruby@1989");
+		driver.findElement(By.name("login")).click();
+		List<WebElement> listProduct = driver.findElements(By.cssSelector(".col-lg-4 "));
+		
+	}
+
+}
